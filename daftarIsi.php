72d9212b4be6576e3c5806cafb02e29438fd33a2
telkomdev-fahrezi <luthfi0071@gmail.com>
2023-12-18 17:34:16 +0700

    Init paper digital assignment

---
+++ daftarIsi.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DAFTAR ISI</title>
+  <link rel="stylesheet" href="./styles/index.css">
+  <link rel="stylesheet" href="./styles/daftarIsi.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+  <h2>DAFTAR ISI</h2>
+  <section>
+    <a class="redirect" href="bab1.php">
+      <h1>Bab 1 (Pendahuluan)</h1>
+    </a>
+    <div>
+      <p>Latar Belakang</p>
+      <p>Tujuan</p>
+    </div>
+  </section>
+  <section>
+    <a class="redirect" href="bab2.php">
+      <h1>Bab 2 (Pembahasan)</h1>
+    </a>
+    <div>
+      <p>Pengertian Web</p>
+      <p>Struktur Navigasi</p>
+      <p>Prototype</p>
+      <p>Hasil</p>
+    </div>
+  </section>
+  <section>
+    <a class="redirect" href="bab3.php">
+      <h1>Bab 3 (Penutup)</h1>
+    </a>
+    <div>
+      <p>Kesimpulan</p>
+      <p>Saran</p>
+    </div>
+  </section>
+  <footer class="navigator">
+    <div class="left">
+      <a href="home.php">
+        <img src="images/arrow-left.png" alt="Utama"> 
+      </a>
+      <p>Utama</p>
+    </div>
+    <div class="right">
+      <a href="bab1.php">
+        <img src="images/arrow-right.png" alt="Bab 1"> 
+      </a>
+      <p>Bab 1</p>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Paper Digital Assignment</title>
+  <link rel="stylesheet" href="./styles/index.css">
+  <link rel="stylesheet" href="./styles/home.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <h1>Makalah</h1>
+    <p>by Website</p>
+  </header>
+  <section>
+    <h2>Kata Pengantar</h2>
+    <p>Puji syukur kami panjatkan kehadirat Tuhan Yang Maha Esa atas segala rahmat-Nya yang telah melimpahkan kepada kami sehingga kami dapat menyelesaikan makalah ini dengan baik. Kami ingin mengucapkan terima kasih kepada Ibu Rizka Dahlia, M.Kom selaku dosen pengampu mata kuliah Web Programming I atas bimbingan dan dukungan yang diberikan selama proses pembelajaran. </p>
+    <p>Project ini disusun sebagai salah satu tugas akhir dalam mata kuliah Web Programming I guna memenuhi syarat kelulusan. Dalam menyusun project ini, penulis berusaha mengintegrasikan pengetahuan yang diperoleh selama perkuliahan dan mengaplikasikannya dalam pengembangan sebuah aplikasi web yang fungsional.</p>
+    <p>Pembuatan web merupakan sebuah langkah signifikan dalam memahami dan mengimplementasikan konsep-konsep yang telah dipelajari dalam mata kuliah Web Programming. Melalui proyek ini, penulis berupaya mengaplikasikan pengetahuan tersebut dengan merancang dan mengembangkan sebuah situs web yang bermanfaat dan sesuai dengan kebutuhan yang diinginkan.</p>
+    <p>Akhir kata, penulis menyampaikan permohonan maaf apabila masih terdapat kekurangan dalam penyusunan project ini. Kritik dan saran yang bersifat membangun sangat diharapkan guna perbaikan di masa yang akan datang. Semoga pembuatan web ini dapat memberikan manfaat dan memberikan inspirasi bagi pengembangan web selanjutnya. Terima kasih kepada semua pihak yang telah mendukung dan membantu dalam proses pembuatan web ini.</p>
+    <div class="text-right">
+      <p>Jakarta, 04 Desember 2023</p>
+      <p>Penulis</p>
+    </div>
+  </section>
+  <footer class="navigator">
+    <div class="right">
+      <a href="daftarIsi.php">
+        <img src="images/arrow-right.png" alt="Daftar Isi"> 
+      </a>
+      <p>Daftar Isi</p>
+    </div>
+  </footer>
+</body>
+</html>
